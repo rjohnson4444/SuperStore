2abfafefbdbf967e7971dacdb5bec9a23f19135b
Ryan Johnson <rjohnson4444@live.com>
2015-02-16 18:49:21 -0700

    Image now showing up dynamically

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,10 @@ class PagesController < ApplicationController
   	if current_user
   	  redirect_to books_path
   	end
+  	@books = Book.last(4)
+  end
 
   def dashboard
     @books = current_user.books
-  end
   end
 end
